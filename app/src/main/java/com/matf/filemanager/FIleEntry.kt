56449214873787882ef078491470d16197b8aee0
aleksa <kojadinovic.aleksa98@gmail.com>
2020-04-18 19:47:13 +0200

    Minor fixes

--- app/src/main/java/com/matf/filemanager/FIleEntry.kt
+++ app/src/main/java/com/matf/filemanager/FIleEntry.kt
@@ -6,7 +6,7 @@ class FileEntry (var file: File, var selected: Boolean){
 
     fun listFileEntries(): ArrayList<FileEntry> {
         var ret: ArrayList<FileEntry> = ArrayList()
-        for (f: File in file.listFiles().sorted()){
+        for (f: File in file.listFiles().sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it.name })){
             ret.add(FileEntry(f, false))
         }
         return ret
