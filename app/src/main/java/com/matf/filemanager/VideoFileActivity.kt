f45a7ce106a66e49df13400a018e42e608ee11bc
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2020-04-18 15:03:36 +0200

    Removed titlebar from video player

--- app/src/main/java/com/matf/filemanager/VideoFileActivity.kt
+++ app/src/main/java/com/matf/filemanager/VideoFileActivity.kt
@@ -13,6 +13,15 @@ class VideoFileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        if (supportActionBar != null)
+            supportActionBar?.hide()
+
         setContentView(R.layout.activity_video_file)
 
         val path = intent.getStringExtra("file_path")
