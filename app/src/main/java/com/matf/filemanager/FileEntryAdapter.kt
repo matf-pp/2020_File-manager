e89d5d1e398c449ef4eb16e0b300711d070da834
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2020-04-23 13:51:22 +0200

    New selection mode layout

--- app/src/main/java/com/matf/filemanager/FileEntryAdapter.kt
+++ app/src/main/java/com/matf/filemanager/FileEntryAdapter.kt
@@ -6,10 +6,9 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.matf.filemanager.manager.FileManager
+import com.matf.filemanager.util.MenuMode
 
 class FileEntryAdapter(context: Context) : BaseAdapter() {
 
@@ -20,27 +19,42 @@ class FileEntryAdapter(context: Context) : BaseAdapter() {
         val view: View = inflater.inflate(R.layout.file_entry, parent, false)
         view.findViewById<TextView>(R.id.fileTitletv).text = FileManager.entries[position].file.name
 
-        val imageView: ImageView = view.findViewById(R.id.icon)
+        val imgIcon: ImageView = view.findViewById(R.id.imgIcon)
+        val bProperties: ImageView = view.findViewById(R.id.bProperties)
+        val cbSelected: CheckBox = view.findViewById(R.id.cbSelected)
 
         if(!FileManager.entries[position].file.isDirectory) {
             when {
-                FileManager.entries[position].file.extension.matches(Regex("^(jpg|jpeg|png|JPG)$")) -> imageView.setImageResource(R.drawable.image)
-                FileManager.entries[position].file.extension.matches(Regex("^(mp4|mkv|webm)$")) -> imageView.setImageResource(R.drawable.music)
-                else -> imageView.setImageResource(R.drawable.text)
+                FileManager.entries[position].file.extension.matches(Regex("^(jpg|jpeg|png|JPG)$")) -> imgIcon.setImageResource(R.drawable.image)
+                FileManager.entries[position].file.extension.matches(Regex("^(mp4|mkv|webm)$")) -> imgIcon.setImageResource(R.drawable.music)
+                else -> imgIcon.setImageResource(R.drawable.text)
             }
         } else {
-            imageView.setImageResource(R.drawable.emptyfolder)
+            imgIcon.setImageResource(R.drawable.emptyfolder)
         }
 
         if (!FileManager.entries[position].file.isDirectory)
             view.findViewById<TextView>(R.id.fileSizetv).text =
-                "size: " + FileManager.entries[position].file.length().toString() + " bytes"
+                view.context.getString(R.string.text_size, FileManager.entries[position].file.length())
         else
             view.findViewById<TextView>(R.id.fileSizetv).text = ""
 
+        when(FileManager.menuMode) {
+            MenuMode.SELECT -> {
+                bProperties.visibility = ImageButton.GONE
+                cbSelected.visibility = CheckBox.VISIBLE
+            }
+            MenuMode.OPEN -> {
+                bProperties.visibility = ImageButton.VISIBLE
+                cbSelected.visibility = CheckBox.GONE
+            }
+        }
+
         if (FileManager.entries[position].selected) {
-            view.setBackgroundColor(Color.DKGRAY)
+            cbSelected.isChecked = true
+            view.setBackgroundColor(view.resources.getColor(R.color.colorHighlight))
         } else {
+            cbSelected.isChecked = false
             view.setBackgroundColor(Color.TRANSPARENT)
         }
         return view
